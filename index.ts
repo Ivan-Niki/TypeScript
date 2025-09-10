c8b4562eb861bfd45f5aacff9d5b1f184f13a9fe
Ivan-Niki <iFankov@gmail.com>
2025-09-11 01:20:29 +0300

    added ts settings and creating of variables example

---
+++ index.ts
@@ -0,0 +1,8 @@
+let userName: string = 'Mark';
+console.log(userName);
+
+// типы данных
+// создание переменных
+const age: number = 20; // к типу number также относятся числа с плавающей точкой (20.74935), NaN, Infinity
+console.log(age);
+
