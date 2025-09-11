a1d87546c27840e7b0ee673aa724400b1a211252
Ivan-Niki <iFankov@gmail.com>
2025-09-11 03:00:41 +0300

    added ts functions typing

--- index.ts
+++ index.ts
@@ -6,3 +6,22 @@ console.log(userName);
 const age: number = 20; // к типу number также относятся числа с плавающей точкой (20.74935), NaN, Infinity
 console.log(age);
 
+const a1: number = 7.5; // NaN, Infinity
+const a2: string = 'hello'; // "hello", `hello`
+const a3: boolean = true; // false
+const a4: null = null;
+const a5: undefined = undefined;
+const a6: any = true;
+
+// типизация функций
+function f1(a: number, b: string, c: boolean): string {
+    return 'hello';
+}
+
+// типизация стрелочных функций
+const f2 = (a: number, b: string, c: boolean): string => {
+    return 'hello world'
+}
+
+
+// типизация объектов
